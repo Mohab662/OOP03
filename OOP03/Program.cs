66cb3156e0f1acfd4fc6e615b27828ff7332bb58
Mohab662 <mohabmbelkan@gmail.com>
2024-07-15 04:54:34 +0300

    Polymorphism - Overloading

--- OOP03/Program.cs
+++ OOP03/Program.cs
@@ -2,9 +2,30 @@
 {
     internal class Program
     {
+
+        #region Method Overloading
+        public static int Sum(int x, int y)
+        {
+            return x * y;
+        }
+        public static int Sum(int x, int y, int z)
+        {
+            return x * y;
+        }
+        public static int Sum(int x, int y, int m, int z)
+        {
+            return x * y;
+        } 
+        #endregion
+
         static void Main(string[] args)
         {
-            
+            #region Overloading Test
+            int Result = Sum(1, 2);
+            Console.WriteLine(Result); 
+            #endregion
+
+
         }
     }
 }
